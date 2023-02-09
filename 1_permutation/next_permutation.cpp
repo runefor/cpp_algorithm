9abdec50e0c1e98e49a19626b80c9b7bdf451f31
runefor <fkjy132@gmail.com>
2023-02-10 01:48:26 +0900

    new file:   .vscode/settings.json 	modified:   1_permutation/next_permutation.cpp 오름차순으로 출력하는 순열과 내림차순으로 출력하는 순열에 대해 공부함 c++로 거의 처음 코딩을 하는데 아직까진 파이썬 쓰는 것처럼 매우 쉬운 것 같다. 내가 알던 C언어의 느낌은 일단 전혀 안난다

--- 1_permutation/next_permutation.cpp
+++ 1_permutation/next_permutation.cpp
@@ -1,7 +1,41 @@
+#include <cstdio>
+#include <algorithm>
+#include <vector>
 #include <iostream>
 
 using namespace std;
 
+
+void printV(vector<int> &v){
+    for(int i = 0; i < v.size(); i++){
+        cout << v[i] << " ";
+    }
+    cout << endl;
+}
+
 int main(){
-    cout << "Hello World!" << endl;
+    // next_permutation은 오름차순의 배열을 기반으로 순열을 만들어준다.
+    // 1, 2, 3부터 오름차순으로 순열을 뽑습니다.
+    int a[3] = {1, 2, 3};
+    vector<int> v;
+
+    for(int i = 0; i < 3; i++)v.push_back(a[i]);
+
+    do{
+        printV(v);
+    }while (next_permutation(v.begin(), v.end()));
+
+    cout << endl << endl;
+
+    // prev_permutation은 내림차순의 배열을 기반으로 순열을 만들어준다.
+    // 3, 2, 1부터 내림차순으로 순열을 뽑습니다.
+    v.clear();
+
+    for(int i = 2; i >= 0; i--)v.push_back(a[i]);
+
+    do{
+        printV(v);
+    }while (prev_permutation(v.begin(), v.end()));
+
+    return 0;
 }
