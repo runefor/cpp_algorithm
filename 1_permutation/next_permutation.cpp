9038184e32ede7ab7e6c279bd1535bb914125c45
runeofor <fkjy132@gmail.com>
2024-09-12 11:43:36 +0900

    update next permutation

--- 1_permutation/next_permutation.cpp
+++ 1_permutation/next_permutation.cpp
@@ -16,26 +16,11 @@ void printV(vector<int> &v){
 int main(){
     // next_permutation은 오름차순의 배열을 기반으로 순열을 만들어준다.
     // 1, 2, 3부터 오름차순으로 순열을 뽑습니다.
-    int a[3] = {1, 2, 3};
-    vector<int> v;
-
-    for(int i = 0; i < 3; i++)v.push_back(a[i]);
+    vector<int> v = {1, 2, 3, 200, 100};
+    sort(v.begin(), v.end());
 
     do{
         printV(v);
     }while (next_permutation(v.begin(), v.end()));
-
-    cout << endl << endl;
-
-    // prev_permutation은 내림차순의 배열을 기반으로 순열을 만들어준다.
-    // 3, 2, 1부터 내림차순으로 순열을 뽑습니다.
-    v.clear();
-
-    for(int i = 2; i >= 0; i--)v.push_back(a[i]);
-
-    do{
-        printV(v);
-    }while (prev_permutation(v.begin(), v.end()));
-
     return 0;
 }
